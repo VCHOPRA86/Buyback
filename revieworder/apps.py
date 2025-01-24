b07a158e7cefdac3a97a1b4969d49e62cb5e25e1
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2025-01-24 16:27:43 +0000

    Update apps.py

--- revieworder/apps.py
+++ revieworder/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RevieworderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'revieworder'
+
+    def ready(self):
+        import revieworder.signals
