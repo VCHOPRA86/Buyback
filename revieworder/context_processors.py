a91d90c2d87941c4d7fa7aa817974e1fa942d234
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2025-01-24 16:27:04 +0000

    Update context_processors.py

--- revieworder/context_processors.py
+++ revieworder/context_processors.py
@@ -1,11 +1,16 @@
-from django.db.models import Sum
 from .models import Cart
 
-# Quantity of items in the cart for the navbar
 def cart_total_quantity(request):
     total_quantity = 0
+    
+    # For authenticated users
     if request.user.is_authenticated:
         total_quantity = Cart.objects.filter(user=request.user).count()
+    
+    # For unauthenticated users (checking the session)
+    elif 'cart' in request.session:
+        total_quantity = len(request.session['cart'])
+
     return {
         'total_quantity': total_quantity
     }
