0fddc34f402a75c9a89153407bedf59acefcfe39
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2025-02-01 11:29:55 +0000

    Update settings.py

--- buyback/settings.py
+++ buyback/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cf*&g7@zb1cv(j_-(1%j658b*_+%!j!mh1419!n#b)6h7z-**='
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -209,15 +209,18 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
-
-EMAIL_HOST = "smtp.gmail.com"  # Use your email provider's SMTP server
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'buymygadgets@gmail.com'  
-EMAIL_HOST_PASSWORD = 'fisxqrffcbbzzrdq'
-
-DEFAULT_FROM_EMAIL = "EMAIL_HOST_USER"
-ADMIN_EMAIL = "vikas.chopra@hotmail.co.uk"  # Set this to the admin's
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = ''
+else:
+    EMAIL_HOST = "smtp.gmail.com"  # Use your email provider's SMTP server
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    
+    DEFAULT_FROM_EMAIL = "EMAIL_HOST_USER"
+    ADMIN_EMAIL = "os.environ.get('EMAIL_HOST_USER')"  # Set this to the admin's
 
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
