c20e709b924c645beec9c3527f81339a155c2979
VCHOPRA86 <150377095+VCHOPRA86@users.noreply.github.com>
2025-02-14 11:07:19 +0000

    Update settings.py

--- buyback/settings.py
+++ buyback/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cashthatgadget-6181349c7e0a.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cashthatgadget-6181349c7e0a.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap4',
     'home',
